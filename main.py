0ec3cff3f03e6495a3263c16eaa31e2916c62d2b
Llampi <ll_elias@hotmail.com>
2023-07-03 00:01:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 import streamlit as st
 from camera_input_live import camera_input_live
 
-"# Detección de rostro"
+"# Streamlit camera input live Demo"
+"## Try holding a qr code in front of your webcam"
 
 image = camera_input_live()
 
@@ -13,17 +14,17 @@ if image is not None:
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
     # Cargar el archivo haarcascade.xml
-    cascade_path = 'haarcascade_frontalface_alt.xml'
+    cascade_path = "haarcascade_frontalface_alt.xml"
     face_cascade = cv2.CascadeClassifier(cascade_path)
 
     # Convertir la imagen a escala de grises
     gray_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
 
     # Detectar objetos usando el clasificador de cascada
-    objects = face_cascade.detectMultiScale(gray_img, scaleFactor=1, minNeighbors=2, minSize=(10, 10))
+    objects = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=3, minSize=(30, 30))
 
     if len(objects) > 0:
-        st.write("### Objetos encontrados ")
+        st.write("# Found object(s)")
         
         for (x, y, w, h) in objects:
             # Dibujar cuadro alrededor del objeto
@@ -34,4 +35,4 @@ if image is not None:
 
         st.image(cv2_img)
     else:
-        st.write("No se detectan objetos.")
+        st.write("No object found in the image.")
