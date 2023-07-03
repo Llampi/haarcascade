fde55004e5fbf7b5b55d93c709e07da6e6363857
Llampi <ll_elias@hotmail.com>
2023-07-02 23:40:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,26 +13,24 @@ if image is not None:
     bytes_data = image.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-    # Cargar el archivo haarcascade.xml
-    cascade_path = "haarcascade.xml"
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    detector = cv2.QRCodeDetector()
 
-    # Convertir la imagen a escala de grises
-    gray_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+    data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
 
-    # Detectar objetos usando el clasificador de cascada
-    objects = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    if data:
+        st.write("# Found QR code")
+        st.write(data)
 
-    if len(objects) > 0:
-        st.write("# Found object(s)")
-        
-        for (x, y, w, h) in objects:
-            # Dibujar cuadro alrededor del objeto
-            cv2.rectangle(cv2_img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # Dibujar cuadro alrededor del QR code
+        if bbox is not None:
+            bbox = np.int0(bbox)  # Convertir a tipo de datos entero
+            cv2.polylines(cv2_img, [bbox], isClosed=True, color=(255, 0, 0), thickness=2)
 
         with st.expander("Show details"):
-            st.write("Object(s) detected:", len(objects))
+            st.write("BBox:", bbox)
+            st.write("Straight QR code:", straight_qrcode)
 
         st.image(cv2_img)
     else:
-        st.write("No object found in the image.")
+        st.write("No QR code found in the image.")
+
