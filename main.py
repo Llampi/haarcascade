aa544b3b1a02869e032980e7d541db88a200269e
Llampi <ll_elias@hotmail.com>
2023-07-02 22:04:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import pandas as pd
 import cv2
 import mediapipe as mp
 import numpy as np
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
+from webcam import webcam
 
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
+st.title("Webcam capture component")
+
+st.write("""
+- Accesses the user's webcam and displays the video feed in the browser.
+- Click the "Capture Frame" button to grab the current video frame and
+return it to Streamlit.
+""")
+captured_image = webcam()
+if captured_image is None:
+    st.write("Waiting for capture...")
 else:
-    st.write('Stopped')
+    st.write("Got an image from the webcam:")
+    st.image(captured_image)
 
 
 
